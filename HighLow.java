53c772d20a4149cdbd2d1cb4da869db4f9cba154
Robinson-erc <109825086+Robinson-erc@users.noreply.github.com>
2022-11-15 00:23:05 -0500

    Add files via upload

--- HighLow.java
+++ HighLow.java
@@ -11,60 +11,77 @@ public class HighLow
     private int m_gamesPlayed;
     private int m_sumofScores;
     private Deck deck;
-    
     public void Play(){
         Scanner in=new Scanner(System.in);
-        System.out.println("Play again?");
-        char answer=in.next().charAt(0);
-        while(PlayAgainPrompt()=='y'||PlayAgainPrompt()=='Y'){
-            //System.out.println("I hate life");
-            int GetScore;
-            GetScore=PlayARound();
-            System.out.println(GetScore);
-            System.out.println("Play again?");
-            answer=in.next().charAt(0);
-            
-         }
-        
-        
-
+        System.out.println("welcome"); 
     }
     protected char PlayAgainPrompt(){
         char answer;
         Scanner in=new Scanner(System.in);
-        System.out.println("Would you like to play again?");
+        System.out.println("Would you like to play again? Y/N");
         answer=in.next().charAt(0);
-        
         return answer;
     }
     protected int PlayARound(){
+        char guess;
         Deck deck=new Deck();
         deck.Shuffle();
         Card currentCard;
-        Card nextCard;
+        Card NextCard;
         currentCard=deck.DealACard();
-        System.out.println(currentCard+" is the current card");
+        NextCard=deck.DealACard();
+        DisplayCurrentCard(currentCard);        
+        guess=GuessPrompt();
+        NextCard(NextCard);
         
-        // deck.DealACard();
-        GuessPrompt();
+        if(NextCard.GetValue()==currentCard.GetValue()){
+           System.out.println("The value is the same as the last card");
+               System.out.println("You lose on ties!"); 
+        }
+        else if(NextCard.GetValue()>currentCard.GetValue()){
+               if(guess=='H'){
+                   System.out.println("Your prediction was correct");
+                   m_gamesPlayed++;
+               }
+               else{
+                   System.out.println("The prediction was incorrect");
+               }
+           }
+        else {
+               if(guess=='L'){
+                   System.out.println("Your prediction was correct");
+                   m_gamesPlayed++;
+               }
+               else{
+                   System.out.println("The prediction was incorrect");      
+               }
+           }    
         
-        return this.m_sumofScores;
+        System.out.println("The games won is "+m_gamesPlayed);
+        return this.m_gamesPlayed;
+        
     }
     protected char GuessPrompt(){
         char prompt;
         Scanner in=new Scanner(System.in);
-        System.out.println("Will the next card be higher or lower than the current card");
+        System.out.println("Will the next card be higher (H) or lower (L)?");
         prompt=in.next().charAt(0);
         return prompt;
     }
     protected void DisplayCurrentCard(Card card){
-        
+        System.out.println("The current card is "+card);
     }
-
+    protected void NextCard(Card card){
+        System.out.println("The next card is "+card);
+    }
     public static void main(String args[]){
         HighLow highlow =new HighLow();
-        highlow.Play();
+        int count=0;
+        do{
+            highlow.PlayARound();
+            count++;
+        }while(highlow.PlayAgainPrompt()=='Y'); 
         
     }
-
+    
 }
